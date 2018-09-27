afadef9330d1bc27bb80ce36f8ecaf643930d406
AbelClown <AbelClown@github.com>
2018-09-27 20:23:30 +0900

    Edit_2

--- arduino/git_module/GIT_module_noclass.ino
+++ arduino/git_module/GIT_module_noclass.ino
@@ -13,6 +13,7 @@
 #define PBuzzer 2
 const long interval = 1000;
 unsigned long previousMillis = 0;
+unsigned long currentMillis = millis();
 int State = 0;
 
 void setup() {
@@ -22,7 +23,7 @@ void setup() {
     pinMode(i, OUTPUT);
 
   pinMode(PBuzzer, OUTPUT);
-  digitalWrite(PBuzzer, LOW);
+  noTone(PBuzzer);
 
   for (int i = 3; i < 7; i++)
     pinMode(i, INPUT);
@@ -31,11 +32,9 @@ void setup() {
 }
 
 void Flash(int blinkLed) {
-  unsigned long currentMillis = millis();
 
   if (currentMillis - previousMillis >= interval) {
 
-    previousMillis = currentMillis;
 
     if (State == LOW) {
       State = HIGH;
@@ -46,6 +45,9 @@ void Flash(int blinkLed) {
     }
     digitalWrite(blinkLed, State);
     digitalWrite(PBuzzer, State);
+    Serial.print(blinkLed);
+    Serial.println(" is working");
+    previousMillis = currentMillis;
 
   }
 }
@@ -55,140 +57,33 @@ void Off(int led) {
 }
 
 void Wrong() {
-  digitalWrite(PBuzzer, HIGH);
+  //신호 보낼 것@!@!@!@
+  tone(PBuzzer, 500);
   delay(200);
-  digitalWrite(PBuzzer, LOW);
+  noTone(PBuzzer);
   delay(200);
-  digitalWrite(PBuzzer, HIGH);
+  tone(PBuzzer, 500);
   delay(200);
-  digitalWrite(PBuzzer, LOW);
+  noTone(PBuzzer);
   delay(200);
-  digitalWrite(PBuzzer, HIGH);
+  tone(PBuzzer, 500);
   delay(200);
-  digitalWrite(PBuzzer, LOW);
+  noTone(PBuzzer);
 }
 
 void Rbut_start() {
   boolean switch1 = 0;
   boolean switch2 = 0;
 
+ while(true){
+   Flash(Rled);//빨강버튼 반짝반짝
+   if (Bbut==0) break;
+   else if (Rbut == 0) {Wrong(); continue;}
+   else if (Gbut == 0) {Wrong(); continue;}
+   else if (Ybut == 0) {Wrong(); continue;}
+ }
+//Off(Rled);
 
-  Flash(Rled); //빨강버튼 반짝반짝
-
-  while (true) {
-    while (true) {
-      if (digitalRead(Bbut) == LOW){ 
-       Off(Rled);
-       break;
-      }
-      else if (digitalRead(Gbut) == LOW) Wrong();
-      else if (digitalRead(Rbut) == LOW) Wrong();
-      else if (digitalRead(Ybut) == LOW) Wrong();
-
-// 1차, 파랑버튼 누르기 완료     
-    }
-    Off(Rled); //빨강끄기
-
-    delay(1000);
-
-    Flash(Gled); //초록 켜기
-    delay(500);
-    Flash(Yled); //노랑 켜기
-    while (true) {
-      if (digitalRead(Rbut) == LOW) {
-        switch1 = 1;
-        continue;
-      } //빨강, 초록순으로 누르기
-      if (switch1 == 1) {
-        if (digitalRead(Gbut) == LOW) break;
-        else if (digitalRead(Rbut) == LOW) {
-          Wrong();
-          switch1 == 0;
-          continue;
-        }
-        else if (digitalRead(Bbut) == LOW) {
-          Wrong();
-          switch1 == 0;
-          continue;
-        }
-        else if (digitalRead(Ybut) == LOW) {
-          Wrong();
-          switch1 == 0;
-          continue;
-        }
-      } else if (digitalRead(Gbut) == LOW) Wrong();
-      else if (digitalRead(Rbut) == LOW) Wrong();
-      else if (digitalRead(Ybut) == LOW) Wrong();
-    }
-    //2차 해제 완료!
-    Off(Gled);
-    Off(Yled);
-
-    delay(1000);
-
-    Flash(Yled);
-    delay(500);
-    Flash(Bled);
-    delay(500);
-    Flash(Rled);
-//세개 빤짝빤짝
-    while (true) //파랑, 빨강 초록 순 누르기
-    {
-      if (digitalRead(Bbut) == LOW) {
-        switch1 = 1;
-        continue;
-      } //파랑
-      if (switch1 == 1) {
-        if (digitalRead(Rbut == LOW)) {
-          switch2 = 1;
-          continue;
-        } //빨강
-        else if (digitalRead(Gbut) == LOW) {
-          Wrong();
-          switch1 == 0;
-          continue;
-        }
-        else if (digitalRead(Bbut) == LOW) {
-          Wrong();
-          switch1 == 0;
-          continue;
-        }
-        else if (digitalRead(Ybut) == LOW) {
-          Wrong();
-          switch1 == 0;
-          continue;
-        }
-      }
-      if (switch1 == 1 && switch2 == 1) {
-        if (digitalRead(Gbut == LOW)) break; //초록
-        else if (digitalRead(Rbut) == LOW) {
-          Wrong();
-          switch1 == 0;
-          switch2 == 0;
-          continue;
-        }
-        else if (digitalRead(Bbut) == LOW) {
-          Wrong();
-          switch1 == 0;
-          switch2 == 0;
-          continue;
-        }
-        else if (digitalRead(Ybut) == LOW) {
-          Wrong();
-          switch1 == 0;
-          switch2 == 0;
-          continue;
-        }
-      } else if (digitalRead(Gbut) == LOW) Wrong();
-      else if (digitalRead(Rbut) == LOW) Wrong();
-      else if (digitalRead(Ybut) == LOW) Wrong();
-    }
-    Off(Rled);
-    Off(Yled);
-    Off(Bled);
-    //Correct() 추가하기
-
-  }
 }
 
 void loop() {
